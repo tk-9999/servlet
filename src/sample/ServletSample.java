7b6f2bcb2e83ca431085aec0042a26be69839084
tkhr <tkhr.22.1@gmail.com>
2020-09-12 08:37:00 +0900

    test

--- src/sample/ServletSample.java
+++ src/sample/ServletSample.java
@@ -9,16 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ServletSample
- */
-@WebServlet("/ServletSample")
+@WebServlet("/Hello")
 public class ServletSample extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ServletSample() {
         super();
     }
@@ -47,7 +41,7 @@ public class ServletSample extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		doGet(request, response);
 	}
 
